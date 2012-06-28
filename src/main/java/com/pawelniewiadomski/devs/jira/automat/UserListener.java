0df636b03b0ca092c51a1708ba0a19fcc5b4c26b
Pawel Niewiadomski <pniewiadomski@atlassian.com>
2012-06-28 23:32:52 +0200

    working on user event listener

--- src/main/java/com/pawelniewiadomski/devs/jira/automat/UserListener.java
+++ src/main/java/com/pawelniewiadomski/devs/jira/automat/UserListener.java
@@ -2,37 +2,46 @@ package com.pawelniewiadomski.devs.jira.automat;
 
 import com.atlassian.jira.event.user.UserEvent;
 import com.atlassian.jira.event.user.UserEventListener;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 
 import java.util.Map;
 
-/**
- * TODO: Document this class / interface here
- *
- * @since v5.0.1
- */
 public class UserListener implements UserEventListener {
+    private final static Logger log = Logger.getLogger(AutomatListener.class);
+
     @Override
     public void userSignup(UserEvent event) {
+        processEvent(event);
+    }
+
+    private void processEvent(UserEvent event) {
+        log.debug(event.toString());
     }
 
     @Override
     public void userCreated(UserEvent event) {
+        processEvent(event);
     }
 
     @Override
     public void userForgotPassword(UserEvent event) {
+        processEvent(event);
     }
 
     @Override
     public void userForgotUsername(UserEvent event) {
+        processEvent(event);
     }
 
     @Override
     public void userCannotChangePassword(UserEvent event) {
+        processEvent(event);
     }
 
     @Override
     public void init(Map params) {
+        log.setLevel(Level.ALL);
     }
 
     @Override
